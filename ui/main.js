3155006ede99986b00dda1feeebe5afa2f47186a
AKASHD1 <222dutta@gmail.com>
2018-03-04 16:51:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,3 +27,18 @@ console.log('Loaded!');
  element2.onclick=function() {
      var interval= setInterval(moveRight,50);
  }
+ 
+ //for counter code
+ var button=document.getElementById('counter');
+ var counter=0;
+ button.click=function(){
+     //make a request to the counter endpoint
+     
+     //capture the response and store it in a variable
+     
+     //render the variable in the correct span
+     counter=counter+1;
+     var span=document.getElementById('count');
+     span.innerHTML=counter.toString();
+     
+ }
