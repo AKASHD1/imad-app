ca34b20908eb705a5abd41260d3edb4a2dc8f258
AKASHD1 <222dutta@gmail.com>
2018-03-04 13:24:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,18 @@ console.log('Loaded!');
  //moving the image with animation
  var element2=document.getElementById('madi');
  var marginLeft=0;
+ var marginRight=0;
  function moveRight()
  {
+     if(marginLeft>20)
+     {
+         marginRight=marginRight+1;
+        element2.style.marginRight=marginRight+'px'; 
+     }
+     else{
      marginLeft=marginLeft+1;
      element2.style.marginLeft=marginLeft+'px';
+     }
  }
  element2.onclick=function() {
      var interval= setInterval(moveRight,50);
