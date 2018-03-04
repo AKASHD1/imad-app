a20667b549272103e3aa173e7f647895956d9758
AKASHD1 <222dutta@gmail.com>
2018-03-04 12:54:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 //changing the content of the html element main-text
  var element=document.getElementById('main-text')
  element.innerHTML="bbye meet you u again";
+ var element2=document.getElementById('madi')
+ element2.onclick=function() {
+     element2.style.marginleft="100px";
+ }
